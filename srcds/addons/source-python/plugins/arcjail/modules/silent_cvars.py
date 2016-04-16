aab16d147ab4a0783f678b542ef6783c29db6183
KirillMysnik <kirill@mysnik.com>
2016-04-16 10:44:02 +0300

    Fixed wrong flag access method

--- srcds/addons/source-python/plugins/arcjail/modules/silent_cvars.py
+++ srcds/addons/source-python/plugins/arcjail/modules/silent_cvars.py
@@ -2,7 +2,7 @@ from cvars.flags import ConVarFlags
 
 
 def silent_set(cvar, type_, value):
-    if cvar.NOTIFY:
+    if cvar.is_flag_set(ConVarFlags.NOTIFY):
         cvar.remove_flags(ConVarFlags.NOTIFY)
         getattr(cvar, 'set_{}'.format(type_))(value)
         cvar.add_flags(ConVarFlags.NOTIFY)

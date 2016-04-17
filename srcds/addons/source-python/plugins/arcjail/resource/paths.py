5408934b6c38a63f5493dbd8468c7d4ab225da77
Kirill Mysnik <kirill@mysnik.com>
2016-04-17 13:09:25 +0300

    Added ARCJAIL_LOG_PATH path

--- srcds/addons/source-python/plugins/arcjail/resource/paths.py
+++ srcds/addons/source-python/plugins/arcjail/resource/paths.py
@@ -1,9 +1,10 @@
-from paths import CFG_PATH, GAME_PATH, PLUGIN_DATA_PATH
+from paths import CFG_PATH, GAME_PATH, LOG_PATH, PLUGIN_DATA_PATH
 
 from ..info import info
 
 
 ARCJAIL_CFG_PATH = CFG_PATH / info.basename
 ARCJAIL_DATA_PATH = PLUGIN_DATA_PATH / info.basename
+ARCJAIL_LOG_PATH = LOG_PATH / info.basename
 DOWNLOADLISTS_PATH = ARCJAIL_CFG_PATH / "downloadlists"
 MAPDATA_PATH = GAME_PATH / "mapdata"

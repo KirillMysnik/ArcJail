69e958091ee218758e0248aba39f08a9aab7f214
Kirill Mysnik <kirill@mysnik.com>
2016-04-22 21:16:57 +0300

    Added GPL headers

--- srcds/addons/source-python/plugins/arcjail/modules/lrs/game_classes/survival.py
+++ srcds/addons/source-python/plugins/arcjail/modules/lrs/game_classes/survival.py
@@ -1,3 +1,18 @@
+# This file is part of ArcJail.
+#
+# ArcJail is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ArcJail is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ArcJail.  If not, see <http://www.gnu.org/licenses/>.
+
 from warnings import warn
 
 from cvars import ConVar

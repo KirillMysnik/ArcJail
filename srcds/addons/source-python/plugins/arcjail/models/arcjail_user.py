9f5a02ee6c7bc7c7a3dc82df2240a891dd06f4ed
Kirill Mysnik <kirill@mysnik.com>
2016-05-01 13:16:23 +0300

    Added ArcjailUser model

---
+++ srcds/addons/source-python/plugins/arcjail/models/arcjail_user.py
@@ -0,0 +1,32 @@
+# This file is part of ArcJail.
+#
+# ArcJail is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ArcJail is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ArcJail.  If not, see <http://www.gnu.org/licenses/>.
+
+from sqlalchemy import Column, Integer, String, Text
+
+from ..resource.config import config
+from ..resource.sqlalchemy import Base
+
+
+class ArcjailUser(Base):
+    __tablename__ = config['database']['prefix'] + "arcjail_users"
+
+    id = Column(Integer, primary_key=True)
+    steamid = Column(String(32))
+
+    account = Column(Integer)
+    slot_data = Column(Text)
+
+    def __repr__(self):
+        return "<ArcjailUser({})>".format(self.id)

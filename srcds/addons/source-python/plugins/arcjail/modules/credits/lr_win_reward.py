cfdfd048ae251d7854b51b13dc2ee3585684c173
Kirill Mysnik <kirill@mysnik.com>
2016-06-25 20:35:02 +0300

    Fixed events in lr_win_reward Added missing GPL headers

--- srcds/addons/source-python/plugins/arcjail/modules/credits/lr_win_reward.py
+++ srcds/addons/source-python/plugins/arcjail/modules/credits/lr_win_reward.py
@@ -1,3 +1,18 @@
+# This file is part of ArcJail.
+#
+# ArcJail is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ArcJail is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ArcJail.  If not, see <http://www.gnu.org/licenses/>.
+
 from ...arcjail import InternalEvent
 
 from ...resource.strings import build_module_strings
@@ -9,6 +24,6 @@ strings_module = build_module_strings('credits/lr_win_reward')
 
 
 @InternalEvent('jail_lr_won')
-def on_jail_lr_won(winner, loser):
-    earn_credits(
-        winner, credits_config['rewards']['lr_win'], strings_module['reason'])
+def on_jail_lr_won(event_var):
+    earn_credits(event_var['winner'], int(credits_config['rewards']['lr_win']),
+                 strings_module['reason'])

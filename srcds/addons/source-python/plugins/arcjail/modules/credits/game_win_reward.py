240088454579b66829955633c1af7aa144bbe7b4
KirillMysnik <kirill@mysnik.com>
2016-06-25 05:38:15 +0300

    Fixed typos, and apparently I didn't know my own InternalEvent class

--- srcds/addons/source-python/plugins/arcjail/modules/credits/game_win_reward.py
+++ srcds/addons/source-python/plugins/arcjail/modules/credits/game_win_reward.py
@@ -5,34 +5,31 @@ from ...resource.strings import build_module_strings
 from . import credits_config, earn_credits
 
 
-strings_module = build_module_strings('credits/lr_win_reward')
+strings_module = build_module_strings('credits/game_win_reward')
 
 
 @InternalEvent('jail_game_cock_fight_winners')
-def on_jail_game_cock_fight_winners(winners, starting_player_number):
+def on_jail_game_cock_fight_winners(event_var):
     payout = int(
-        credits_config['rewards']['cock_fight_win'] *
-        (1 - len(winners) / starting_player_number)
+        int(credits_config['rewards']['cock_fight_win']) *
+        (1 - len(event_var['winners']) / event_var['starting_player_number'])
     )
 
-    for winner in winners:
+    for winner in event_var['winners']:
         earn_credits(winner, payout, strings_module['reason cock_fight'])
 
 
 @InternalEvent('jail_game_chat_game_winner')
-def on_jail_game_chat_game_winner(winner):
-    earn_credits(winner, credits_config['rewards']['chat_game_win'],
-                 strings_module['reason chat_game'])
+def on_jail_game_chat_game_winner(event_var):
+    earn_credits(
+        event_var['winner'], int(credits_config['rewards']['chat_game_win']),
+        strings_module['reason chat_game'])
 
 
 @InternalEvent('jail_game_map_game_team_based_winners')
-def on_jail_game_map_game_team_based_win(
-        winners, num_teams, starting_player_number, team_num):
-
-    payout = int(
-        credits_config['rewards']['map_game_team_based_win'] *
-        (1 - len(winners) / (starting_player_number // num_teams))
-    )
+def on_jail_game_map_game_team_based_win(event_var):
+    winners = event_var['winners']
+    payout = int(credits_config['rewards']['map_game_team_based_win'])
 
     for winner in winners:
         earn_credits(
@@ -40,9 +37,12 @@ def on_jail_game_map_game_team_based_win(
 
 
 @InternalEvent('jail_game_map_game_winners')
-def on_jail_game_map_game_winners(winners, starting_player_number):
+def on_jail_game_map_game_winners(event_var):
+    winners = event_var['winners']
+    starting_player_number = event_var['starting_player_number']
+
     payout = int(
-        credits_config['rewards']['cock_fight_win'] *
+        int(credits_config['rewards']['cock_fight_win']) *
         (1 - len(winners) / starting_player_number)
     )
 

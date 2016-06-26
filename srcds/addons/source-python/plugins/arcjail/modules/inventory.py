6ba3f3000a300d949968ccbcb00ad12b09801129
Kirill Mysnik <kirill@mysnik.com>
2016-06-26 06:14:10 +0300

    Added "Immediate Item Activation Popup" feature

--- srcds/addons/source-python/plugins/arcjail/modules/inventory.py
+++ srcds/addons/source-python/plugins/arcjail/modules/inventory.py
@@ -16,15 +16,79 @@
 from spam_proof_commands.client import ClientCommand
 from spam_proof_commands.say import SayCommand
 
+from menus import PagedMenu, PagedOption
 from players.entity import Player
+
+from ..resource.strings import build_module_strings
+
+from .arcjail.arcjail_user import arcjail_user_manager
 
 from .motd.inventory import send_page
 
+from .players import tell
+
 
 ANTI_SPAM_TIMEOUT = 2
+
+
+strings_module = build_module_strings('inventory')
+
+
+def popup_select_callback(popup, player_index, option):
+    item = option.value
+    player = item.player
+
+    reason = item.class_.try_activate(player, item.amount - 1)
+    if reason is not None:
+        tell(player, strings_module['chat_popup_error'].tokenize(text=reason))
+
+    else:
+        arcjail_user = arcjail_user_manager[player.index]
+        arcjail_user.take_item(item, amount=1, async=True)
+
+        tell(
+            player, strings_module['chat_popup_notify'].tokenize(
+                text=strings_module['popup_notify activated'].tokenize(
+                    caption=item.class_.caption
+                )
+            )
+        )
+
+    send_popup(player)
+
+
+def send_popup(player):
+    arcjail_user = arcjail_user_manager[player.index]
+
+    popup = PagedMenu(select_callback=popup_select_callback,
+                      title=strings_module['popup title'])
+
+    for item in arcjail_user.iter_all_items():
+        if player.team not in item.class_.use_team_restriction:
+            continue
+
+        if not item.class_.manual_activation:
+            continue
+
+        popup.append(PagedOption(
+            text=strings_module['popup entry'].tokenize(
+                caption=item.class_.caption, amount=item.amount),
+            value=item,
+        ))
+
+    if not popup:
+        popup.title = strings_module['popup empty_message']
+
+    popup.send(player.index)
 
 
 @ClientCommand(ANTI_SPAM_TIMEOUT, ['inventory', 'inv'])
 @SayCommand(ANTI_SPAM_TIMEOUT, ['!inventory', '!inv'])
 def command_inventory(command, index, team_only=None):
     send_page(Player(index))
+
+
+@ClientCommand(ANTI_SPAM_TIMEOUT, ['inventory_text', 'inv_text'])
+@SayCommand(ANTI_SPAM_TIMEOUT, ['!inventory_text', '!inv_text'])
+def command_inventory_text(command, index, team_only=None):
+    send_popup(Player(index))
